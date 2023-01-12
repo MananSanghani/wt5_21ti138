574e70f847b6c29e204264174c2ef32449b57658
MananSanghani <121211217+MananSanghani@users.noreply.github.com>
2023-01-12 11:07:54 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,7 @@
+function d(){
+    var currentdate = new Date();
+    let disp =  document.getElementById('disp');
+    disp.innerText = currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+}
+setInterval(d, 1000);
+
